4374605b9d1221a3bf0fccd44d939ea74943f176
Ruth Huey <rhuey@scripps.edu>
2004-01-07 19:52:33 +0000

    merged with /mgl/aduser1/src/autogrid/ag3.
    CVS-ID: autoglobal.h 1.2

--- autoglobal.h
+++ autoglobal.h
@@ -39,7 +39,7 @@ char    AutoDockHelp[] = "           -p parameter_filename\n           -l log_fi
 char    AutoGridHelp[] = "-p parameter_filename\n-l log_filename\n-o (old PDBQ format)\n-d (increment debug level)\n-u (display this message)\n";
 
 char    dock_param_fn[MAX_CHARS];
-char    grid_param_fn[MAX_CHARS];
+char    GPF_filename[MAX_CHARS];
 
 int     command_mode = FALSE;
 int     debug = 0;
@@ -54,7 +54,7 @@ float	idct = 1.;
 FILE    *command_in_fp;
 FILE    *command_out_fp;
 FILE    *parFile;
-FILE    *GPF;
+FILE    *GPF_fileptr;
 FILE    *logFile;
 
 /*
