eb528cdfa804ad08947bf66af0f1db710109189d
Ruth Huey <rhuey@scripps.edu>
2004-01-09 00:27:14 +0000

    restored type_ct_ptr to dynamically count number of atomtypes in receptor.
    CVS-ID: get_atom_type.c 1.4
    CVS-ID: get_atom_type.c 1.4

--- get_atom_type.c
+++ get_atom_type.c
@@ -11,15 +11,18 @@ extern FILE *logFile;
 
 
 int get_atom_type( char aname[4], 
-		   char chtype[MAX_TYPES] )
+		   char chtype[MAX_TYPES],
+           int * type_ct_ptr)
 
 {
     char ch, ch1;
     register int i; 
     int type = -1;
+    int type_ct;
 
     /* DEBUG (void) fprintf (stderr, "DEBUG: aname='%s', chtype='%s'\n", aname, chtype); */
 
+    type_ct = *type_ct_ptr;
     ch1 = aname[0];
     /* DEBUG (void) fprintf (stderr, "DEBUG: get_atom_type: ch1= '%c'\n", ch1); */
 
@@ -39,7 +42,7 @@ int get_atom_type( char aname[4],
     /* ch = (ch1 == ' ') ? aname[1] : ch1; */
 
     /* DEBUG (void) fprintf (stderr, "DEBUG: get_atom_type: ch= '%c'\n", ch); */
-    for ( i=0; i<MAX_TYPES; i++) {
+    for ( i=0; i<type_ct; i++) {
         /* DEBUG (void) fprintf (stderr, "DEBUG: get_atom_type: i= %d\n", i); */
         if (ch == chtype[i]) {
             type = i;
@@ -47,6 +50,16 @@ int get_atom_type( char aname[4],
             break; /* break out of this for i loop */
         }
     }    
+    if (type == -1) {
+        /*append it to chtype and increment type_ct*/
+        type = type_ct;
+        chtype[type_ct++] = ch;
+        for ( i=0; i<type_ct; i++) {
+        };
+        *type_ct_ptr = type_ct;
+    }
+
+    /**type_ct_ptr = type_ct;*/
     if (type == -1) {
         (void) fprintf(stderr, "\n%s: WARNING!  Atom type error, can't find type for \"%s\" in typelist \"%s\".\n", programname, aname, chtype );
         (void) fprintf(logFile, "\n%s: WARNING!  Atom type error, can't find type for \"%s\" in typelist \"%s\".\n", programname, aname, chtype );
