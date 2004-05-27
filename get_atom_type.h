1c4c5cd7a77d5f2ec53cfd6cbfcc99265ca7e36b
cvs-fast-export <cvs-fast-export>
2004-05-27 23:29:56 +0000

    Synthetic commit for incomplete tag autogrid4_may04 CVS-ID: printhms.c 1.1.1.1

--- get_atom_type.h
+++
@@ -1,9 +0,0 @@
-#ifndef GET_ATOM_TYPE
-#define GET_ATOM_TYPE
-
-#include "autocomm.h"
-
-int  get_atom_type( char  aname[4], 
-                    char  chtype[MAX_TYPES],
-                    int  * type_ct_ptr);
-#endif
