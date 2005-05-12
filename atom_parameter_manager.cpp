415e570b14ddfa585b5880a549dbde2f1e96862d
Ruth Huey <rhuey@scripps.edu>
2005-05-12 21:51:01 +0000

    replaces use of hseard
    CVS-ID: atom_parameter_manager.c 1.1
    CVS-ID: atom_parameter_manager.cpp 1.1

---
+++ atom_parameter_manager.cpp
@@ -0,0 +1,23 @@
+#define MAXKEY (256*256)
+#include <stdlib.h>
+#include <string.h>
+#include "parameters.h" // needed for ParameterEntry structure
+typedef ParameterEntry PE;
+static PE *dictionary[MAXKEY];
+static unsigned int hash(const char key[]) {
+    switch (strlen(key)) {
+        case 0: return 0;
+        case 1: return (unsigned int)key[0];
+        default: return (unsigned int)key[0] + 256*(unsigned int)key[1];
+    }
+}
+void apm_enter(const char key[], PE value) {
+    if (dictionary[hash(key)] == NULL) {
+        dictionary[hash(key)] = (PE *) calloc(1, sizeof(PE));
+    }
+    *(dictionary[hash(key)]) = value;  // this replaces, as well as inserts
+    return;
+}
+PE * apm_find(const char key[]) {
+    return dictionary[hash(key)];
+}
