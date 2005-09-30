55cde579e113900b71968e4ec54570379baa52c4
Rick Alther <alther@us.ibm.com>
2005-09-30 22:45:06 +0000

    * Added CVS id line * If WIN32 system, include "times.h" instead of <sys/times.h>
    CVS-ID: timesys.h 1.1.6.1

--- timesys.h
+++ timesys.h
@@ -1,9 +1,17 @@
-
+/*
+  $Id: timesys.h,v 1.1.6.1 2005/09/30 22:45:06 alther Exp $
+*/
 
 #ifndef TIMESYS
 #define TIMESYS
 #include <sys/types.h>
-#include <sys/times.h>
+
+#ifdef _WIN32
+   #include "times.h"
+#else
+   #include <sys/times.h>
+#endif
+
 #include <time.h>
 #include "autocomm.h"
 void  timesys( Clock  duration,
