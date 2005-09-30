c90d4f4d9002df61ec8e591798828394adba4b99
Rick Alther <alther@us.ibm.com>
2005-09-30 22:44:21 +0000

    * Added CVS id line * Added multiple include check.
    CVS-ID: times.h 1.1.6.1

--- times.h
+++ times.h
@@ -1,16 +1,23 @@
+/*
+  $Id: times.h,v 1.1.6.1 2005/09/30 22:44:21 alther Exp $
+*/
+
+#ifndef TIMES_H
+#define TIMES_H
 
 #include <time.h>
 
 struct tms
 {
-	clock_t tms_utime;	// CPU time used in executing the instructions of the calling process. 
-	clock_t tms_stime;	// CPU time used by the system on behalf of the calling process. 
-	clock_t tms_cutime;	// sum of the tms_utime values and the tms_cutime values of all terminated child processes of the calling process, whose status has been reported to the parent process by wait or waitpid; see section Process Completion. In other words, it represents the total CPU time used in executing the instructions of all the terminated child processes of the calling process, excluding child processes which have not yet been reported by wait or waitpid. 
-	clock_t tms_cstime;	// similar to tms_cutime, but represents the total CPU time used by the system on behalf of all the terminated child processes of the calling process. 
-	// All of the times are given in clock ticks. These are absolute values; in a newly created process, they are all zero. See section Creating a Process. 
+	clock_t tms_utime;	// CPU time used in executing the instructions of the calling process.
+	clock_t tms_stime;	// CPU time used by the system on behalf of the calling process.
+	clock_t tms_cutime;	// sum of the tms_utime values and the tms_cutime values of all terminated child processes of the calling process, whose status has been reported to the parent process by wait or waitpid; see section Process Completion. In other words, it represents the total CPU time used in executing the instructions of all the terminated child processes of the calling process, excluding child processes which have not yet been reported by wait or waitpid.
+	clock_t tms_cstime;	// similar to tms_cutime, but represents the total CPU time used by the system on behalf of all the terminated child processes of the calling process.
+	// All of the times are given in clock ticks. These are absolute values; in a newly created process, they are all zero. See section Creating a Process.
 };
 
 
 extern clock_t times (struct tms *buffer);
 
+#endif
 // end
