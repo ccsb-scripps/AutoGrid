9968338f7305052f66b707ad3b5a38b694b8799d
Ruth Huey <rhuey@scripps.edu>
2006-07-19 17:11:47 +0000

    replaced FloatOrDouble by Real
    CVS-ID: ranlib.h 1.2

--- ranlib.h
+++ ranlib.h
@@ -5,37 +5,37 @@
 #include "typedefs.h"
 
 extern void advnst(FourByteLong k);
-extern FloatOrDouble genbet(FloatOrDouble aa,FloatOrDouble bb);
-extern FloatOrDouble genchi(FloatOrDouble df);
-extern FloatOrDouble genexp(FloatOrDouble av);
-extern FloatOrDouble genf(FloatOrDouble dfn, FloatOrDouble dfd);
-extern FloatOrDouble gengam(FloatOrDouble a,FloatOrDouble r);
-extern void genmn(FloatOrDouble *parm,FloatOrDouble *x,FloatOrDouble *work);
-extern void genmul(FourByteLong n,FloatOrDouble *p,FourByteLong ncat,FourByteLong *ix);
-extern FloatOrDouble gennch(FloatOrDouble df,FloatOrDouble xnonc);
-extern FloatOrDouble gennf(FloatOrDouble dfn, FloatOrDouble dfd, FloatOrDouble xnonc);
-extern FloatOrDouble gennor(FloatOrDouble av,FloatOrDouble sd);
+extern Real genbet(Real aa,Real bb);
+extern Real genchi(Real df);
+extern Real genexp(Real av);
+extern Real genf(Real dfn, Real dfd);
+extern Real gengam(Real a,Real r);
+extern void genmn(Real *parm,Real *x,Real *work);
+extern void genmul(FourByteLong n,Real *p,FourByteLong ncat,FourByteLong *ix);
+extern Real gennch(Real df,Real xnonc);
+extern Real gennf(Real dfn, Real dfd, Real xnonc);
+extern Real gennor(Real av,Real sd);
 extern void genprm(FourByteLong *iarray,int larray);
-extern FloatOrDouble genunf(FloatOrDouble low,FloatOrDouble high);
+extern Real genunf(Real low,Real high);
 extern void getsd(FourByteLong *iseed1,FourByteLong *iseed2);
 extern void gscgn(FourByteLong getset,FourByteLong *g);
-extern FourByteLong ignbin(FourByteLong n,FloatOrDouble pp);
-extern FourByteLong ignnbn(FourByteLong n,FloatOrDouble p);
+extern FourByteLong ignbin(FourByteLong n,Real pp);
+extern FourByteLong ignnbn(FourByteLong n,Real p);
 extern FourByteLong ignlgi(void);
-extern FourByteLong ignpoi(FloatOrDouble mu);
+extern FourByteLong ignpoi(Real mu);
 extern FourByteLong ignuin(FourByteLong low,FourByteLong high);
 extern void initgn(FourByteLong isdtyp);
 extern FourByteLong mltmod(FourByteLong a,FourByteLong s,FourByteLong m);
 extern void phrtsd(char* phrase,FourByteLong* seed1,FourByteLong* seed2);
-extern FloatOrDouble ranf(void);
+extern Real ranf(void);
 extern void setall(FourByteLong iseed1,FourByteLong iseed2);
 extern void setant(FourByteLong qvalue);
-extern void setgmn(FloatOrDouble *meanv,FloatOrDouble *covm,FourByteLong p,FloatOrDouble *parm);
+extern void setgmn(Real *meanv,Real *covm,FourByteLong p,Real *parm);
 extern void setsd(FourByteLong iseed1,FourByteLong iseed2);
-extern FloatOrDouble sexpo(void);
-extern FloatOrDouble sgamma(FloatOrDouble a);
-extern FloatOrDouble snorm(void);
-extern FloatOrDouble rcauchy(FloatOrDouble, FloatOrDouble);
-extern FloatOrDouble scauchy1(void);
+extern Real sexpo(void);
+extern Real sgamma(Real a);
+extern Real snorm(void);
+extern Real rcauchy(Real, Real);
+extern Real scauchy1(void);
 
 #endif
